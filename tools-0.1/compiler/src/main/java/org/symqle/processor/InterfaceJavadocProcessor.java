8bba426824e8be20204fe74a78ceb608defb23a7
Alexander Izyurov <aizyurov@symqle.org>
2014-03-13 14:22:53 +0000

    javadocs added

--- tools-0.1/compiler/src/main/java/org/symqle/processor/InterfaceJavadocProcessor.java
+++ tools-0.1/compiler/src/main/java/org/symqle/processor/InterfaceJavadocProcessor.java
@@ -3,6 +3,7 @@ package org.symqle.processor;
 import org.symqle.model.InterfaceDefinition;
 import org.symqle.model.Model;
 import org.symqle.model.ModelException;
+import org.symqle.model.TypeParameters;
 import org.symqle.util.Utils;
 
 import java.util.List;
@@ -26,11 +27,20 @@ public class InterfaceJavadocProcessor extends ModelProcessor {
             final String name = def.getType().getSimpleName();
             final List<String> rules = model.getRules(name);
             if (rules != null) {
-                final String javadoc = Utils.format(rules,
-                        "/**" + LINE_BREAK + " *<pre> " + LINE_BREAK + " * " + name + " ::=" + LINE_BREAK + " *          ",
+                final StringBuilder javadocBuilder = new StringBuilder();
+                javadocBuilder.append("/**").append(LINE_BREAK)
+                        .append(" * Represents a ").append(name).append(" symbol of SQL grammar.");
+                javadocBuilder.append(LINE_BREAK);
+                javadocBuilder.append(Utils.format(rules,
+                        " *<pre>" + LINE_BREAK + " * " + name + " ::=" + LINE_BREAK + " *          ",
                         LINE_BREAK + " *        | ",
-                        LINE_BREAK + " *</pre>" + LINE_BREAK + " */" + LINE_BREAK);
-                def.replaceComment(javadoc);
+                        LINE_BREAK + " *</pre>" + LINE_BREAK));
+                final TypeParameters typeParameters = def.getTypeParameters();
+                if (typeParameters.size() == 1) {
+                    javadocBuilder.append("* @param ").append(typeParameters).append(" associated Java type").append(Utils.LINE_BREAK);
+                }
+                javadocBuilder.append(" */").append(LINE_BREAK);
+                def.replaceComment(javadocBuilder.toString());
             }
         }
     }
